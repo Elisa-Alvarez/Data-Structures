424140ec5378e0923906785e3744a8dd70586070
bekah <elisaalvarez211@gmail.com>
2020-09-09 13:23:22 -0700

    Double Linked List

--- Data_Structures_Questions.md
+++ Data_Structures_Questions.md
@@ -3,55 +3,55 @@ Answer the following questions for each of the data structures you implemented a
 ## Stack
 
 1. What is the runtime complexity of `push` using a list?
-
+     O(1) constant
 2. What is the runtime complexity of `push` using a linked list?
-
+    O(1)
 3. What is the runtime complexity of `pop` using a list?
-
+    O(n) linear
 4. What is the runtime complexity of `pop` using a linked list?
-
+    O(n)
 5. What is the runtime complexity of `len` using a list?
-
+    O(n)
 6. What is the runtime complexity of `len` using a linked list?
-
+    O(n)
 ## Queue
 
 1. What is the runtime complexity of `enqueue` using a list?
-
+    O(1)
 2. What is the runtime complexity of `enqueue` using a linked list?
-
+    O(1)
 3. What is the runtime complexity of `dequeue` using a list?
-
+    O(n)
 4. What is the runtime complexity of `dequeue` using a linked list?
-
+    O(n)
 5. What is the runtime complexity of `len` using a list?
-
+    O(n)
 6. What is the runtime complexity of `len` using a linked list?
-
+    O(n)
 ## Doubly Linked List
 
 1. What is the runtime complexity of `ListNode.insert_after`?
-
+    O(n)
 2. What is the runtime complexity of `ListNode.insert_before`?
-
+    O(1)
 3. What is the runtime complexity of `ListNode.delete`?
-
+    O(n)
 4. What is the runtime complexity of `DoublyLinkedList.add_to_head`?
-
+    O(1)
 5. What is the runtime complexity of `DoublyLinkedList.remove_from_head`?
-
+    O(n)
 6. What is the runtime complexity of `DoublyLinkedList.add_to_tail`?
-
+    O(1)
 7. What is the runtime complexity of `DoublyLinkedList.remove_from_tail`?
-
+    O(1)
 8. What is the runtime complexity of `DoublyLinkedList.move_to_front`?
-
+    O(n)
 9. What is the runtime complexity of `DoublyLinkedList.move_to_end`?
-
+    O(n)
 10. What is the runtime complexity of `DoublyLinkedList.delete`?
-
+    O(n)
     a. Compare the runtime of the doubly linked list's `delete` method with the worst-case runtime of the JS `Array.splice` method. Which method generally performs better?
-
+     The linked list's performs beter because it does not exponetally double input*2 
 ## Binary Search Tree
 
 1. What is the runtime complexity of `insert`? 
--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -27,7 +27,18 @@ class DoublyLinkedList:
     the old head node's previous pointer accordingly.
     """
     def add_to_head(self, value):
-        pass
+        new_node = ListNode(value, None, None)
+        self.length += 1
+        if self.head is None and self.tail is None:
+            self.head = new_node
+            self.tail = new_node
+            
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+            
+
         
     """
     Removes the List's current head node, making the
@@ -35,15 +46,26 @@ class DoublyLinkedList:
     Returns the value of the removed Node.
     """
     def remove_from_head(self):
-        pass
-            
+           value = self.head.value
+           self.delete(self.delete)
+           return value
     """
     Wraps the given value in a ListNode and inserts it 
     as the new tail of the list. Don't forget to handle 
     the old tail node's next pointer accordingly.
     """
     def add_to_tail(self, value):
-        pass
+        new_node = ListNode(value, None, None)
+        self.length += 1
+        if self.head is None and self.tail is None:
+            self.head = new_node
+            self.tail = new_node
+           
+        else:
+            new_node.prev = self.tail
+            self.tail.next = new_node
+            self.tail = new_node
+           
             
     """
     Removes the List's current tail node, making the 
@@ -51,32 +73,60 @@ class DoublyLinkedList:
     Returns the value of the removed Node.
     """
     def remove_from_tail(self):
-        pass
-            
+           value = self.tail.value
+           self.delete(self.delete)
+           return value
     """
     Removes the input node from its current spot in the 
     List and inserts it as the new head node of the List.
     """
     def move_to_front(self, node):
-        pass
-        
+        self.delete(node)
+        self.add_to_head(node.value)    
     """
     Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List.
     """
     def move_to_end(self, node):
-        pass
-
+        self.delete(node)
+        self.add_to_tail(node.value)
     """
     Deletes the input node from the List, preserving the 
     order of the other elements of the List.
     """
     def delete(self, node):
-        pass
-
+        if self.length == 0:
+            return None
+        elif self.length == 1:
+            self.head = None
+            self.tail = None
+            self.length = 0
+        elif self.head is node:
+            self.head = node.next
+            self.length = self.length - 1
+        elif self.tail is node:
+            self.tail = node.prev
+            self.length = self.length - 1
+        else:
+            node.next.prev = node.prev
+            node.prev.next = node.next
+            node.next = None
+            node.prev = None
+            self.length = self.length - 1
     """
     Finds and returns the maximum value of all the nodes 
     in the List.
     """
     def get_max(self):
-        pass
+        if not self.head:
+            return None
+        current_max = self.head.value
+        current_node = self.head
+        while current_node:
+            if current_node.value > current_max:
+                current_max = current_node.value
+
+        
+            current_node = current_node.next
+        
+        return current_max
